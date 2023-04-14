d0cd09ac01c5da13996ba6644cd68759336b41f3
kunal <kunalaage21@gmail.com>
2023-04-14 16:52:26 +0530

    fdeat: add toaster

--- src/component/auth/AuthDetails.jsx
+++ src/component/auth/AuthDetails.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../../firebase";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const AuthDetails = () => {
   const [authUser, setAuthUser] = useState(null);
@@ -22,8 +25,12 @@ const AuthDetails = () => {
     signOut(auth)
       .then(() => {
         console.log("sign out successful");
+        toast.success("You have been signed out.");
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        toast.error("Error signing out.");
+      });
   };
   return (
     <div>
