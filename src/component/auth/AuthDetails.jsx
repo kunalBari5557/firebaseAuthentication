31d45e2abb546af30b780ea5818db548aad5f038
kunal <kunalaage21@gmail.com>
2023-04-14 17:02:15 +0530

    fix: toast remove

--- src/component/auth/AuthDetails.jsx
+++ src/component/auth/AuthDetails.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../../firebase";
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
 
 const AuthDetails = () => {
   const [authUser, setAuthUser] = useState(null);
@@ -25,12 +22,8 @@ const AuthDetails = () => {
     signOut(auth)
       .then(() => {
         console.log("sign out successful");
-        toast.success("You have been signed out.");
       })
-      .catch((error) => {
-        console.log(error);
-        toast.error("Error signing out.");
-      });
+      .catch((error) => console.log(error));
   };
   return (
     <div>
