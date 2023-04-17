a24f8cd2d0686d451cc2bbe33787e6182928a07e
kunal <kunalaage21@gmail.com>
2023-04-17 11:23:45 +0530

    fix: firebase.js file

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyASdcmZMfEOSVYYqIaQds-axacbFaecpvk",
   authDomain: "fir-auth-51ed2.firebaseapp.com",
